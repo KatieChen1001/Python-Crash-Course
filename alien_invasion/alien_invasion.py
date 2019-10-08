8a678bd2e46de2146367b4f56b4152a4ea020e30
katiechen1001 <mzc219@nyu.edu>
2019-10-08 15:53:04 +0800

    alien game can move, one ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,27 +1,38 @@
+"""
+Process
+
+1. create canvas w. pygame 
+2. class Settings
+3. create class "ship"
+	- pass in the screen size into class "ship"
+4. Refactoring: 
+	- create game_function module
+	- move check events into game_function module
+5. Let user move ship (in game_functions)
+6. let user move ship continueously
+
+"""
+
 import sys
 import pygame 
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#Initialize game and create a screen object 
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	
-	#set the background color 
-	bg_color = (230, 230, 230)
+	# Make a ship
+	ship = Ship(ai_settings, screen)
 	
 	#Start the main loop for the game 
 	while True:
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)
 		
-		#Watch for keyboard and mouse events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-		
-		#Redraw the screen during each pass through the loop
-		screen.fill(bg_color)
-		
-		#Make the most recently drawn screen visible
-		pygame.display.flip()
-
 run_game()
