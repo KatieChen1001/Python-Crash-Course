c622546104c56dbf4134b24295b311c8b55e1aab
katiechen1001 <mzc219@nyu.edu>
2019-10-29 13:48:12 +0800

    dict DONE, add save to JSON

--- review_log.py
+++ review_log.py
@@ -1,5 +1,5 @@
 
-taglist = []       
+taglist = {}       
 
 typing = True
 
@@ -10,30 +10,11 @@ while typing:
 		typing = False
 		print("existing program")
 		break
-
-	if not taglist: 
-		print("empaty array")
-		new_tag = {}
-		new_tag['tagName'] = new_tag_name
-		new_tag['counter'] = 1
-		taglist.append(new_tag)
-		print(taglist)
 	
+	if new_tag_name in taglist:
+		taglist[new_tag_name] += 1 
 	else: 
-		
-		for index in range(len(taglist)):
-			for key in taglist[index]: 
-				print(taglist[index][key])
-		
-		# for tags in taglist: 
-			# if tags['tagName'] == new_tag_name: 
-				# print("this tag already exists")
-				# count = int(tags['counter'])
-				# tags['counter'] = count + 1
-				# print(taglist)
-			# else: 
-				# print("got a new tag")
-				# taglist.append({'tagName': new_tag_name, 'counter': 1})
-				# break
+		taglist[new_tag_name] = 1
+	print(taglist)
 	
 		
