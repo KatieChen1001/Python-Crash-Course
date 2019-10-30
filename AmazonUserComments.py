4ca578f44b59779a91bfb5e0c56a01db78e75241
katiechen1001 <mzc219@nyu.edu>
2019-10-30 19:15:11 +0800

    scraper script

---
+++ AmazonUserComments.py
@@ -0,0 +1,33 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import time
+import random
+
+
+fakeUA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11'
+url = 'https://www.amazon.com/Microsoft-3RA-00022-Surface-Ergonomic-Keyboard/product-reviews/B01MFCTRZM/ref=cm_cr_getr_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews&pageNumber='
+totalPages = 3
+
+fakeheader = {}
+fakeheader['User-agent'] = fakeUA
+
+for page in range(1,totalPages): 
+    r = requests.get(url+str(page), timeout=30, headers=fakeheader)
+    r.raise_for_status
+    r.encoding = r.apparent_encoding
+
+
+    soup1 = BeautifulSoup(r.content,'html.parser')
+    resTitle = soup1.find_all(attrs={"data-hook":"review-title"})
+    resDate = soup1.find_all(attrs={"data-hook":"review-date"})
+    resBody = soup1.find_all(attrs={"data-hook":"review-body"})
+    print(resTitle)
+    print(resDate)
+
+    # for i in range(3, len(resTitle)):
+        # print(">>Title: "+ resTitle[i].string)
+        # print(">>Date: " + resDate[i-2].string)
+        # print(">>Body: " + resBody[i-2].text)
+
+    time.sleep(random.randint(5,9))
--- review_log.py
+++ review_log.py
@@ -1,20 +1,33 @@
+import json
 
-taglist = {}       
+data = {}
 
+try: 
+	data_file = open('taglist.json', 'r+')
+	data = json.load(data_file)
+except IOError as e: 
+	print(e)
+	print("something went wrong")
+	
+	
 typing = True
 
 while typing: 
-	new_tag_name = input("New Tag Name = ")
+	input_tag_name = input("New Tag Name = ")
+	new_tag_name = input_tag_name.lower()
 	
 	if new_tag_name == 'q':
 		typing = False
-		print("existing program")
+		json.dump(data, data_file, indent=4, sort_keys=True)
+		data_file.close()
+		print("closing file and existing program...")
 		break
-	
-	if new_tag_name in taglist:
-		taglist[new_tag_name] += 1 
+
+	if new_tag_name in data:
+		data[new_tag_name] += 1
+		print(data)
 	else: 
-		taglist[new_tag_name] = 1
-	print(taglist)
+		data[new_tag_name] = 1
+		print(data)
 	
-		
+	
